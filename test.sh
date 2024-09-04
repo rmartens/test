f34973e0047f502153fa5b34193a78502c141524
Rob Martens <rmartens@gmail.com>
2024-09-04 14:35:36 -0500

    init

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e # errors
+set -x # debugging
+set -u # exit if using undeclared variable
+
+# start
+echo "******************************************************"
+echo "******************************************************"
+echo "************* START $0 **************************"
+
+# get location of script file before starting
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo "DIR=$DIR"
+
+# add shared functions
+# source $DIR/sharedFunctions
+
+
+main()
+{
+    echo "main"
+
+}
+
+main
+
+exit 0
